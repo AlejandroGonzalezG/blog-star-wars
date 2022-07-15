2f87e91d03844322f5a8a386dd3d609328d805f3
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-07-15 15:13:20 +0000

    avance ejercicio star wars

--- src/Views/People.jsx
+++ src/Views/People.jsx
@@ -1,8 +1,55 @@
+import React from 'react'
+
+
+const estiloCard = {
+  width: "100%"
+}
+
+
 const People = () => {
   return (
-    <>
-      <h1>People goes here</h1>
-    </>
+    <div className='container'>
+      <div className="card mb-3" style={estiloCard}>
+        <div className="row g-0">
+          <div className="col-md-8">
+            <img src="https://s.yimg.com/ny/api/res/1.2/iEvfx3t.A3307FS0iMH.bA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTQ4MA--/https://s.yimg.com/uu/api/res/1.2/ZjF63mKLcXfJ_7d9Xm48ZA--~B/aD03MjA7dz0xNDQwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/es/esquire_spain_51/1f275d2dfe4c7631713e5e15fcb0294f" className="img-fluid rounded-start" alt="..." ></img>
+          </div>
+          <div className="col-md-4">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-md-3'>
+          Physical characteristics
+        </div>
+        <div className='col-md-2'>
+          Birth-year
+        </div>
+        <div className='col-md-2'>
+          Gender
+        </div>
+        <div className='col-md-1'>
+          HomeWorld
+        </div>
+        <div className='col-md-1'>
+          Films
+        </div>
+        <div className='col-md-1'>
+          Species
+        </div>
+        <div className='col-md-1'>
+          Vehicles
+        </div>
+        <div className='col-md-1'>
+          Starships
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/Views/Vehicles.jsx
+++ src/Views/Vehicles.jsx
@@ -1,8 +1,47 @@
+import React from 'react'
+
+const estiloCard = {
+    width: "100%"
+}
+
 const Vehicles = () => {
   return (
-    <>
-      <h1>Vehicles goes here</h1>
-    </>
+    <div className='container'>
+            <div className="card mb-3" style={estiloCard}>
+                <div className="row g-0">
+                    <div className="col-md-8">
+                        <img src="https://w7.pngwing.com/pngs/347/128/png-transparent-luke-skywalker-landspeeder-skywalker-family-jedi-youtube-youtube-outdoor-shoe-mode-of-transport-vehicle.png" className="img-fluid rounded-start" alt="..." ></img>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-md-3'>
+                    Model
+                </div>
+                <div className='col-md-2'>
+                    Manufacturer
+                </div>
+                <div className='col-md-3'>
+                    Characteristics
+                </div>
+                <div className='col-md-2'>
+                    Vehicle Class
+                </div>
+                <div className='col-md-1'>
+                    Pilots
+                </div>
+                <div className='col-md-1'>
+                    Films
+                </div>
+            </div>
+        </div>
   );
 };
 
