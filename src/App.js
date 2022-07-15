b20b0ea62b6983a78d3de1a9800de3d5f8bcd8c1
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-15 10:23:37 -0400

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import injectContext from './store/appContext';
 import Navbar from './Components/Navbar';
+import NotFound from './Views/NotFound';
+import Home from './Views/Home';
+import People from './Views/People';
+import Vehicles from './Views/Vehicles';
+import Planets from './Views/Planets';
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/category/people" element={<People />} />
+          <Route path="/category/vehicles" element={<Vehicles />} />
+          <Route path="/category/planets" element={<Planets />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/Views/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Hi, home here!</h1>
+    </>
+  );
+};
+
+export default Home;
