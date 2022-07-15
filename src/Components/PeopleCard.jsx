9c30949b07f757db08c12e7fdd0469a4914ddc36
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-15 16:53:57 -0400

    Update PeopleCard Component

--- src/Components/PeopleCard.jsx
+++ src/Components/PeopleCard.jsx
@@ -1,19 +1,58 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 const PeopleCard = ({ name, height, mass, gender }) => {
-    return (
-        <>
-            <div className="card" style={{ width: '18rem' }}>
-                <img src="https://aux.iconspalace.com/uploads/204257333216454877.png" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <ul>
-                        <li>{height}</li>
-                        <li>{mass}</li>
-                        <li>{gender}</li>
-                    </ul>
-                </div>
-            </div>
-        </>
-    );
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+  return (
+    <>
+      <div className="card" style={{ width: '18rem' }}>
+        <img
+          src="https://aux.iconspalace.com/uploads/204257333216454877.png"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <ul className="person-details">
+            <li>{height}</li>
+            <li>{mass}</li>
+            <li>{gender}</li>
+          </ul>
+          <div className="btns-container d-flex">
+            <Link to="/" className="btn btn-primary">
+              Learn More!
+            </Link>
+            {isActive ? (
+              <button className="btn btn-secondary" onClick={handleClick}>
+                <i className="fa-solid fa-heart"></i>
+              </button>
+            ) : (
+              <button className="btn btn-secondary" onClick={handleClick}>
+                <i className="fa-regular fa-heart"></i>
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* <div className="card" style={{ width: '18rem' }}>
+        <img
+          src="https://aux.iconspalace.com/uploads/204257333216454877.png"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <ul>
+            <li>{height}</li>
+            <li>{mass}</li>
+            <li>{gender}</li>
+          </ul>
+        </div>
+      </div> */}
+    </>
+  );
 };
 export default PeopleCard;
-
