cb818339151b941d83c93a4a3f2b8014ee77d6a4
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-15 16:23:49 -0400

    Demo homepage

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,19 +1,55 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import Card from '../Components/Card';
 import { Context } from '../store/appContext';
+import '../Styles/Home.css';
 
 const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
     <>
-      <h1>Hi, home here!</h1>
-
-      <div className="d-flex">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="people mt-5">
+        <div className="header-container d-flex align-items-center">
+          <h1 className="category-title">People</h1>
+          <button className="btn btn-more">
+            <Link to="/category/people">Learn More :)</Link>
+          </button>
+        </div>
+        <div className="d-flex">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+      <div className="vehicles mt-5">
+        <div className="header-container d-flex align-items-center">
+          <h1 className="category-title">Vehicles</h1>
+          <button className="btn btn-more">
+            <Link to="/category/vehicles">Learn More :)</Link>
+          </button>
+        </div>
+        <div className="d-flex">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+      <div className="planets mt-5">
+        <div className="header-container d-flex align-items-center">
+          <h1 className="category-title">Planets</h1>
+          <button className="btn btn-more">
+            <Link to="/category/planets">Learn More :)</Link>
+          </button>
+        </div>
+        <div className="d-flex">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </>
   );
